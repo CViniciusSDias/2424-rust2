db3bfa190cf7ee31ab826554232b16f808634a1c
Vinicius Dias <vinicius@venngage.com>
2022-01-22 17:53:05 -0300

    Video 1.2

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
 fn main() {
-    println!("Hello, world");
+    let notas: [f32; 4] = [10.0, 8.0, 9.5, 6.0];
+
+    println!(
+        "Nota1 = {}, Nota2 = {}, Nota3 = {}, Nota4 = {}",
+        notas[0],
+        notas[1],
+        notas[2],
+        notas[3]
+    );
 }
