d03a8a1bbac96914637492eb963017b3aa8a8e28
Vinicius Dias <vinicius@venngage.com>
2022-01-22 18:22:58 -0300

    Video 2.1

--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,27 @@ fn main() {
     }
 
     matriz();
+    println!("É fim de semana? {}", eh_fim_de_semana(DiaDaSemana::Quarta));
+
+    let dia: DiaDaSemana = DiaDaSemana::Sexta;
+}
+
+enum DiaDaSemana {
+    Domingo,
+    Segunda,
+    Terca,
+    Quarta,
+    Quinta,
+    Sexta,
+    Sabado
+}
+
+fn eh_fim_de_semana(dia_da_semana: DiaDaSemana) -> bool
+{
+    match dia_da_semana {
+        DiaDaSemana::Domingo | DiaDaSemana::Sabado => true,
+        _ => false
+    }
 }
 
 fn matriz() {
