535984877171dd799193fd770a45c4f6d52dfc3f
Vinicius Dias <vinicius@venngage.com>
2022-01-22 18:12:30 -0300

    Video 1.3

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,25 @@
 fn main() {
-    let notas: [f32; 4] = [10.0, 8.0, 9.5, 6.0];
+    let notas: [f32; 4] = [6.5; 4];
+    let inteiro: usize = 0;
 
-    println!(
-        "Nota1 = {}, Nota2 = {}, Nota3 = {}, Nota4 = {}",
-        notas[0],
-        notas[1],
-        notas[2],
-        notas[3]
-    );
+    println!("{}", notas[inteiro]);
+
+    for indice in 0..notas.len() {
+        println!("A nota {} é = {}", indice + 1, notas[indice]);
+    }
+
+    matriz();
+}
+
+fn matriz() {
+    let matriz: [[f32; 3]; 2] = [
+        [0.0, 1.2, 0.1],
+        [1.3, 0.3, 1.4]
+    ];
+
+    for linha in matriz {
+        for item in linha {
+            println!("Item = {}", item);
+        }
+    }
 }
