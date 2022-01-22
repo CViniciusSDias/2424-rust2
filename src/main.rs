3ab966add868de1e37078d796c14f2dd7075abdb
Vinicius Dias <vinicius@venngage.com>
2022-01-22 18:47:00 -0300

    Video 2.3

--- src/main.rs
+++ src/main.rs
@@ -11,11 +11,10 @@ fn main() {
     matriz();
     println!("É fim de semana? {}", eh_fim_de_semana(DiaDaSemana::Quarta));
 
-    let dia: DiaDaSemana = DiaDaSemana::Sexta;
-
     cores();
 }
 
+#[allow(dead_code)]
 enum DiaDaSemana {
     Domingo,
     Segunda,
@@ -47,21 +46,28 @@ fn matriz() {
     }
 }
 
+#[allow(dead_code)]
 enum Color {
     Red,
     Green,
     Blue,
-    RgbColor(u8, u8, u8)
+    RgbColor(u8, u8, u8),
+    CymkColor{cyan: u8, magenta: u8, yellow: u8, black: u8}
 }
 
 fn cores() {
-    let cor = Color::Red;
+    let cor = Color::RgbColor(12, 5, 32);
 
     println!("Cor = {}", match cor {
         Color::Red => "vermelho",
         Color::Green => "verde",
         Color::Blue => "blue",
-        Color::RgbColor(0, 0, 0) => "preta",
-        Color::RgbColor(_, _, _) => "RGB desconhecido"
+        Color::RgbColor(0, 0, 0)
+            | Color::CymkColor{cyan: _, magenta: _, yellow: _, black: 255} => "preta",
+        Color::RgbColor(_, green, _) => {
+            println!("{}", green);
+            "RGB desconhecido"
+        }
+        Color::CymkColor{cyan: _, magenta: _, yellow: _, black: _} => "CYMK desconhecido"
     });
 }
